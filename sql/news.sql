fce593476d99607559bf02140b1c7ae5d4e3c443
DimkoChe <dm.chernyshov@gmail.com>
2013-07-04 01:59:43 +0400

    fixes #1 Схема БД

--- sql/news.sql
+++ sql/news.sql
@@ -3,80 +3,135 @@
  Source Server Type    : MySQL
  Source Server Version : 50529
  Source Host           : localhost
- Source Database       : news
+ Source Database       : linkstore
 
  Target Server Type    : MySQL
  Target Server Version : 50529
  File Encoding         : utf-8
 
- Date: 04/07/2013 17:00:08 PM
+ Date: 07/04/2013 01:53:56 AM
 */
 
 SET NAMES utf8;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
---  Table structure for `likes`
+--  Table structure for `articles`
 -- ----------------------------
-DROP TABLE IF EXISTS `likes`;
-CREATE TABLE `likes` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int(10) unsigned NOT NULL,
-  `news_id` int(10) unsigned NOT NULL,
-  `is_liked` tinyint(1) unsigned NOT NULL,
-  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+DROP TABLE IF EXISTS `articles`;
+CREATE TABLE `articles` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `url` varchar(1024) NOT NULL,
+  `title` varchar(1024) DEFAULT NULL,
+  `description` text,
+  `published` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  KEY `news_id` (`news_id`),
-  CONSTRAINT `likes_news_id` FOREIGN KEY (`news_id`) REFERENCES `news` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `idx_articles-url` (`url`(255)) USING BTREE,
+  KEY `idx-articles-published` (`published`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
---  Table structure for `news`
+--  Table structure for `articles_locations`
 -- ----------------------------
-DROP TABLE IF EXISTS `news`;
-CREATE TABLE `news` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `source_id` int(10) unsigned NOT NULL,
-  `url` varchar(1024) NOT NULL,
-  `title` varchar(1024) NOT NULL,
-  `description` text NOT NULL,
-  `published` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+DROP TABLE IF EXISTS `articles_locations`;
+CREATE TABLE `articles_locations` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_article_id` int(11) unsigned NOT NULL,
+  `location_type` enum('article','browser','source') NOT NULL,
+  `location` varchar(1024) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `source_id` (`source_id`),
-  CONSTRAINT `news_source_id` FOREIGN KEY (`source_id`) REFERENCES `source` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+  KEY `idx-articles_locations-user_article_id` (`user_article_id`) USING BTREE,
+  CONSTRAINT `fk-articles_locations-user_article_id` FOREIGN KEY (`user_article_id`) REFERENCES `user_articles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
---  Table structure for `reads`
+--  Table structure for `sources`
 -- ----------------------------
-DROP TABLE IF EXISTS `reads`;
-CREATE TABLE `reads` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int(10) unsigned NOT NULL,
-  `news_id` int(10) unsigned NOT NULL,
-  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  KEY `news_id` (`news_id`),
-  CONSTRAINT `reads_news_id` FOREIGN KEY (`news_id`) REFERENCES `news` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-
--- ----------------------------
---  Table structure for `source`
--- ----------------------------
-DROP TABLE IF EXISTS `source`;
-CREATE TABLE `source` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `sources`;
+CREATE TABLE `sources` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `type` enum('feed','twitter') NOT NULL DEFAULT 'feed',
   `title` varchar(512) NOT NULL,
   `url` varchar(1024) NOT NULL,
-  `is_active` tinyint(1) unsigned NOT NULL DEFAULT '1',
+  `last_update` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
---  Records of `source`
+--  Table structure for `user_articles`
+-- ----------------------------
+DROP TABLE IF EXISTS `user_articles`;
+CREATE TABLE `user_articles` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL,
+  `article_id` int(11) unsigned NOT NULL,
+  `weigth` tinyint(2) unsigned DEFAULT '0',
+  `is_read` tinyint(3) NOT NULL DEFAULT '0',
+  `read_time` timestamp NULL DEFAULT NULL,
+  `is_liked` tinyint(3) NOT NULL DEFAULT '0',
+  `like_time` timestamp NULL DEFAULT NULL,
+  `source_count` tinyint(3) unsigned DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uniq-user_articles` (`user_id`,`article_id`) USING BTREE,
+  KEY `idx-user_article-user_id` (`user_id`) USING BTREE,
+  KEY `idx-user_article-article_id` (`article_id`) USING BTREE,
+  KEY `idx-user_articles-weigth` (`weigth`) USING BTREE,
+  CONSTRAINT `fk-users_articles-article_id` FOREIGN KEY (`article_id`) REFERENCES `articles` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk-users_articles-user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `user_opml`
+-- ----------------------------
+DROP TABLE IF EXISTS `user_opml`;
+CREATE TABLE `user_opml` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned DEFAULT NULL,
+  `opml` text,
+  `is_handled` tinyint(4) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `idx-user_opml-user_id` (`user_id`) USING BTREE,
+  CONSTRAINT `fk-user_opml-user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `user_sources`
+-- ----------------------------
+DROP TABLE IF EXISTS `user_sources`;
+CREATE TABLE `user_sources` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL,
+  `source_id` int(11) unsigned NOT NULL,
+  `is_active` tinyint(1) unsigned NOT NULL DEFAULT '1',
+  `read_count` int(11) NOT NULL DEFAULT '0',
+  `like_count` int(11) NOT NULL DEFAULT '0',
+  `category` varchar(64) NOT NULL DEFAULT 'No category',
+  PRIMARY KEY (`id`),
+  KEY `idx-users_sources-user_id` (`user_id`) USING BTREE,
+  KEY `idx-users_sources-source_id` (`source_id`) USING BTREE,
+  KEY `idx-users_sources-is_active` (`is_active`) USING BTREE,
+  KEY `idx-user_sources-user_id-source_id` (`user_id`,`source_id`) USING BTREE,
+  KEY `idx-user_sources-read_count-like_count` (`is_active`,`read_count`,`like_count`) USING BTREE,
+  CONSTRAINT `fk-users_sources-source_id` FOREIGN KEY (`source_id`) REFERENCES `sources` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk-users_sources-user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `users`
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Records of `users`
 -- ----------------------------
 BEGIN;
-INSERT INTO `source` VALUES ('1', 'Хабрахабр / Лучшие за сутки / Посты', 'http://habrahabr.ru/rss/best/', '1'), ('2', 'Slender Means', 'http://slendrmeans.wordpress.com/feed/', '1'), ('3', 'Wes McKinney\'s Blog', 'http://wesmckinney.com/blog/?feed=rss2', '1'), ('4', 'Joel on Software', 'http://joelonsoftware.com/rss.xml', '1'), ('5', 'Greg Reda', 'http://www.gregreda.com/feeds/all.atom.xml', '1'), ('6', 'Marco.org', 'http://www.marco.org/rss', '1');
+INSERT INTO `users` VALUES ('1', 'Test');
 COMMIT;
+
 
 SET FOREIGN_KEY_CHECKS = 1;
